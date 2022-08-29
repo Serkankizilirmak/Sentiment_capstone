757f3d48fe9061ed93b59eb599b55cc1ff17037d
Serkankizilirmak <serkankizilirmaak@gmail.com>
2022-08-30 01:13:59 +0300

    18.08.2022

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,7 +68,7 @@ def give_largest(col, n):
 
     index = [f'{i}_largest' for i in range(1, len(largest) + 1)]
 
-    return pd.Series(data, index=index)
+    return pd.DataFrame(data, index=index)
 
 def n_largest(df, axis, n):
 
